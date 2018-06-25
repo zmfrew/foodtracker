a1177e84bd5a05fee1d7ee8c908340f9e9e953bf
zfrew <zmfrew@gmail.com>
2018-06-25 16:24:04 -0500

    add foodtracker app

---
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -0,0 +1,46 @@
+//
+//  FoodTrackerTests.swift
+//  FoodTrackerTests
+//
+//  Created by Zachary Frew on 6/18/18.
+//  Copyright © 2018 Zachary Frew. All rights reserved.
+//
+
+import XCTest
+@testable import FoodTracker
+
+class FoodTrackerTests: XCTestCase {
+    
+    // MARK: Meal Class Tests
+    
+    // Confirm that the Meal Initializer returns a Meal object when passed valid parameters.
+    func testMealInitializationSucceeds() {
+        
+        // Zero rating.
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        // Highest positive rating.
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+        
+    }
+    
+    // Confirm that the Meal Initializer returns nil when passed a negative rating or an empty name.
+    func testMealInitializationFails() {
+        
+        // Negative rating.
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        // Rating exceeds maximum
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+        
+        // Empty string.
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
+        
+    }
+    
+}
